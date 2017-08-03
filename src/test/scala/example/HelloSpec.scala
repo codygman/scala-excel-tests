7c1c2039d58e493ab1df975d5c6a3b62856ba931
Cody Goodman <codygman.consulting@gmail.com>
2017-08-02 23:47:25 -0500

    write to file to inspect sheet order

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -2,6 +2,7 @@ package example
 
 import org.scalatest._
 import info.folone.scala.poi._
+import scalaz._
 
 class HelloSpec extends FlatSpec with Matchers {
   "The addSheetToWorkbook function" should "preserve workbook's sheet order" in {
@@ -11,6 +12,7 @@ class HelloSpec extends FlatSpec with Matchers {
                              )
     }
     val newSheet = Sheet("name3") { Set( Row(0) { Set(StringCell(0, "data")) } ) }
+    Hello.addSheetToWorkbook(workbook, newSheet).safeToFile("/tmp/test.xls").fold(ex ⇒ throw ex, identity).unsafePerformIO
     Hello.addSheetToWorkbook(workbook, newSheet) shouldEqual Workbook {
       Set(Sheet("name") { Set(Row(0) { Set(StringCell(0, "data")) }) },
           Sheet("name2") { Set(Row(0) { Set(StringCell(0, "data")) }) },
